b1ffe5d4d1285fa805b0bdd048670a6b3566d78b
Miguel Pachon <miguelpachon@gmail.com>
2026-05-25 07:24:21 -0500

    eliminacion de basura md y otras cosas

--- backend/make_instructor_admin_nullable.sql
+++
@@ -1,4 +0,0 @@
--- Migración para hacer instructorAdminId nullable
--- Esto permite que las fichas puedan quedar sin líder designado
-
-ALTER TABLE "Ficha" ALTER COLUMN "instructorAdminId" DROP NOT NULL;
